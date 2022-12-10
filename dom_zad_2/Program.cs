a0273414de09b32885213bb93358f423c74c6290
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-12-10 15:17:23 +0300

    домашнее задание 2

---
+++ dom_zad_2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int num = inputNumber();
+System.Console.WriteLine(SumNumbers(num));
+
+
+
+
+
+int SumNumbers(int number)
+{
+    int count = 0;
+    while (number > 9)
+    {
+        count += number % 10;
+        number /= 10;
+    }
+    return count + number;
+}
+
+int inputNumber()
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        Console.WriteLine("Введите число");
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
